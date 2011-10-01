5b30631e2d6b07f4ac9369dbbfb70d85ad03a48d
Ewan Mellor <ewan.mellor@citrix.com>
2011-09-30 17:50:41 -0700

    Revert "create capability to retrieve roles using keystone extension"
    This takes us back to a slightly older version of keystone.
    
    This reverts commit 75b15633b2dc85515118f7c0f5df8c268d677538.

--- openstackx/extras/__init__.py
+++ openstackx/extras/__init__.py
@@ -8,7 +8,6 @@ from openstackx.extras.tenants import TenantManager
 from openstackx.extras.users import UserManager
 from openstackx.extras.usage import UsageManager
 from openstackx.extras.role_refs import RoleRefManager
-from openstackx.extras.roles import RoleManager
 from openstackx.extras.endpoint_templates import EndpointTemplateManager
 from openstackx.extras.endpoints import EndpointManager
 from openstackx.extras.security_groups import SecurityGroupManager
@@ -95,7 +94,6 @@ class Account(object):
         self.tenants = TenantManager(self)
         self.users = UserManager(self)
         self.role_refs = RoleRefManager(self)
-        self.roles = RoleManager(self)
         self.endpoint_templates = EndpointTemplateManager(self)
         self.endpoints = EndpointManager(self)
 
